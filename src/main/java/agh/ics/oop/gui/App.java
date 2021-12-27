75d9942e7f455c2e00aae96d44dd8be75328896a
Uiskam <92760953+Uiskam@users.noreply.github.com>
2021-12-27 17:47:07 +0100

    menu is working

--- src/main/java/agh/ics/oop/gui/App.java
+++ src/main/java/agh/ics/oop/gui/App.java
@@ -45,25 +45,43 @@ public class App extends Application implements IAnimalMoved {
         Scene menuScene = new Scene(this.menuWindow, 600, 600);
         primaryStage.setScene(menuScene);
         primaryStage.show();
+        guiButtons.get(GuiButtons.START.getIndex()).setOnAction(event -> {
+            int height, width, startEnergy, moveEnergy, plantEnergy, initialNumberOfAnimals;
+            boolean magicModeWrapped, magicModeRect, inputDataError = false;
+            double jungleRatio;
+            Label errorText;
+            String tmp;
+            try{
+                tmp = getData(MenuParamEntrance.HEIGHT);
+                height = Integer.parseInt(tmp);
+                tmp = getData(MenuParamEntrance.WIDTH);
+                width = Integer.parseInt(tmp);
+                tmp = getData(MenuParamEntrance.JUNGLE_RATIO);
+                jungleRatio = Double.parseDouble(tmp);
+                tmp = getData(MenuParamEntrance.START_ENERGY);
+                startEnergy = Integer.parseInt(tmp);
+                tmp = getData(MenuParamEntrance.MOVE_ENERGY);
+                moveEnergy = Integer.parseInt(tmp);
+                tmp = getData(MenuParamEntrance.PLANT_ENERGY);
+                plantEnergy = Integer.parseInt(tmp);
+                tmp = getData(MenuParamEntrance.INITIAL_ANIMAL_NUMBER);
+                initialNumberOfAnimals = Integer.parseInt(tmp);
+                magicModeWrapped = ((CheckBox)menuParamEntrance.get(MenuParamEntrance.BORDERLESS_MAP_CHECK.getIndex()).getChildren().get(1)).isSelected();
+                magicModeRect = ((CheckBox)menuParamEntrance.get(MenuParamEntrance.ENCLOSED_MAP_CHECK.getIndex()).getChildren().get(1)).isSelected();
 
-        guiButtons.get(GuiButtons.START.getIndex()).setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                int height, width, startEnergy, moveEnergy, plantEnergy, initialNumberOfAnimals;
-                boolean magicModeWrapped, magicModeRect, inputDataError = false;
-                double jungleRatio;
-                /*do {
-                    //TextField tmp = new TextField();
-                    Node input = menuParamEntrance.get(MenuParamEntrance.HEIGHT.getIndex()).getChildren().get(1);
-                    if(input instanceof TextField){
-                        ((TextField) input).getText()
-                    }
-                    String tmp =
-                }while (inputDataError);*/
-                Label label  = new Label("DUPA");
-                Scene simScene = new Scene(label,100,100);
-                primaryStage.setScene(simScene);
+            } catch (NumberFormatException err){
+                out.println("HERE ");
+                errorText = new Label("All values except for jungle ratio (which is type is double . as separator) MUST be positive integers\n" +
+                        "Tex fields CANNOT contain any character apart from digits, this includes white character\n" +
+                        "All fileds MUST be filled with wanted values" +
+                        "Please restart program and input correct data\n");
+                Scene errorScene = new Scene(errorText,700,100);
+                primaryStage.setScene(errorScene);
+                return;
             }
+            Label label  = new Label("DUPA");
+            Scene simScene = new Scene(label,100,100);
+            primaryStage.setScene(simScene);
         });
     }
 
@@ -119,4 +137,12 @@ public class App extends Application implements IAnimalMoved {
         return menuBox;
     }
 
+    private String getData(MenuParamEntrance input) throws IllegalArgumentException{
+        Node node = menuParamEntrance.get(input.getIndex()).getChildren().get(1);
+        if(node instanceof TextField){
+            return ((TextField) node).getText();
+        } else {
+            throw new IllegalArgumentException("TEXT FIELD NOT FOUND");
+        }
+    }
 }
